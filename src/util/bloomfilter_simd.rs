da72faca859bc4493ba197759ea773dd5a3755a3
Wangweizhen <hawking.rei@gmail.com>
2018-11-04 22:22:09 +0800

    init

--- src/util/bloomfilter_simd.rs
+++ src/util/bloomfilter_simd.rs
@@ -35,6 +35,29 @@ struct Bucket {
     //} bucketData;
 }
 
+/// A BloomFilter stores sets of items and offers a query operation indicating whether or
+/// not that item is in the set.  BloomFilters use much less space than other compact data
+/// structures, but they are less accurate: for a small percentage of elements, the query
+/// operation incorrectly returns true even when the item is not in the set.
+///
+/// When talking about Bloom filter size, rather than talking about 'size', which might be
+/// ambiguous, we distinguish two different quantities:
+///
+/// 1. Space: the amount of buffer pool memory used
+///
+/// 2. NDV: the number of unique items that have been inserted
+///
+/// BloomFilter is implemented using block Bloom filters from Putze et al.'s "Cache-,
+/// Hash- and Space-Efficient Bloom Filters". The basic idea is to hash the item to a tiny
+/// Bloom filter the size of a single cache line or smaller. This implementation sets 8
+/// bits in each tiny Bloom filter. This provides a false positive rate near optimal for
+/// between 5 and 15 bits per distinct value, which corresponds to false positive
+/// probabilities between 0.1% (for 15 bits) and 10% (for 5 bits).
+///
+/// Our tiny BloomFilters are 32 bytes to take advantage of 32-byte SIMD in newer Intel
+/// machines. 'noexcept' is added to various functions called from the cross-compiled code
+/// so LLVM will not generate exception related code at their call sites.
+#[derive(Clone)]
 pub struct Bloomfilter {
     /// log_num_buckets_ is the log (base 2) of the number of buckets in the directory.
     log_num_buckets_: u8,
@@ -43,6 +66,17 @@ pub struct Bloomfilter {
     directory_mask_: u32,
 
     directory_: Vec<[i64; 4]>,
+}
+
+impl Default for Bloomfilter {
+    /// Creates an empty `Bloomfilter`.
+    fn default() -> Bloomfilter {
+        Bloomfilter {
+            log_num_buckets_: 0,
+            directory_mask_: 0,
+            directory_: Vec::new(),
+        }
+    }
 }
 
 impl Bloomfilter {
@@ -71,11 +105,25 @@ impl Bloomfilter {
         }
     }
 
-    #[inline(always)]
-    #[cfg(target_feature = "avx2")]
-    fn insert(self, hash: u32) {
+    fn bucket_find_avx2(&self, bucket_idx: u32, hash: u32) -> bool {
+        unsafe {
+            let mask = make_mask(hash);
+            let addr = self.directory_.as_ptr().offset(bucket_idx as isize);
+            let bucket = _mm256_load_si256(addr as *const __m256i);
+            let result = _mm256_testc_si256(bucket, mask);
+            _mm256_zeroupper();
+            return result == 0;
+        }
+    }
+
+    pub fn insert(&mut self, hash: u32) {
         let bucket_idx = rehash32to32(hash) & self.directory_mask_;
         self.bucket_insert_avx2(bucket_idx, hash)
+    }
+
+    pub fn find(self, hash: u32) -> bool {
+        let bucket_idx = rehash32to32(hash) & self.directory_mask_;
+        return self.bucket_find_avx2(bucket_idx, hash);
     }
 }
 
@@ -96,5 +144,20 @@ fn make_mask(hash: u32) -> __m256i {
         hash_data = _mm256_mullo_epi32(rehash, hash_data);
         hash_data = _mm256_srli_epi32(hash_data, 27);
         return _mm256_sllv_epi32(ones, hash_data);
+    }
+}
+
+#[test]
+fn test_bloom_simd() {
+    let mut bf: Bloomfilter = Default::default();
+    bf = bf.init(8);
+    {
+        //bf.insert(0x47b2137b);
+        //assert!(&bf.find(0x47b2137b) == false);
+        assert!(bf.find(0x4) == true);
+        //bf.insert(0x48ab59e2);
+        //{
+        //
+        //}
     }
 }
