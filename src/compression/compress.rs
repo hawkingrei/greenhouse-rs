5543121f7e0a5f6baedb88a0140246cf4e0ac208
Wangweizhen <hawking.rei@gmail.com>
2018-12-02 20:45:43 +0800

    style: rm useless

--- src/compression/compress.rs
+++ src/compression/compress.rs
@@ -193,7 +193,7 @@ impl Codec for ZSTDCodec {
     }
 
     fn compress(&mut self, input_buf: &[u8], output_buf: &mut Vec<u8>) -> Result<()> {
-        let mut encoder = zstd::stream::encode_all(output_buf, ZSTD_COMPRESSION_LEVEL)?;
+        let mut encoder = zstd::Encoder::new(output_buf, ZSTD_COMPRESSION_LEVEL)?;
         encoder.write_all(&input_buf[..])?;
         match encoder.finish() {
             Ok(_) => Ok(()),
