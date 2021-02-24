d975e01976261877c228243773c9b67a08897af1
wangweizhen <wangweizhen@bilibili.com>
2021-02-24 11:34:16 +0800

    code format

--- components/cibo_util/src/collections.rs
+++ components/cibo_util/src/collections.rs
@@ -2,29 +2,15 @@
 use std::cmp::Eq;
 use std::hash::Hash;
 
-pub type HashMap<K, V> = std::collections::HashMap<
-    K,
-    V,
-    std::hash::BuildHasherDefault<fxhash::FxHasher>,
->;
-pub type HashSet<T> = std::collections::HashSet<
-    T,
-    std::hash::BuildHasherDefault<fxhash::FxHasher>,
->;
+pub type HashMap<K, V> =
+    std::collections::HashMap<K, V, std::hash::BuildHasherDefault<fxhash::FxHasher>>;
+pub type HashSet<T> = std::collections::HashSet<T, std::hash::BuildHasherDefault<fxhash::FxHasher>>;
 pub use std::collections::hash_map::Entry as HashMapEntry;
 
 pub fn hash_set_with_capacity<T: Hash + Eq>(capacity: usize) -> HashSet<T> {
-    HashSet::with_capacity_and_hasher(
-        capacity,
-        fxhash::FxBuildHasher::default(),
-    )
+    HashSet::with_capacity_and_hasher(capacity, fxhash::FxBuildHasher::default())
 }
 
-pub fn hash_map_with_capacity<K: Hash + Eq, V>(
-    capacity: usize,
-) -> HashMap<K, V> {
-    HashMap::with_capacity_and_hasher(
-        capacity,
-        fxhash::FxBuildHasher::default(),
-    )
+pub fn hash_map_with_capacity<K: Hash + Eq, V>(capacity: usize) -> HashMap<K, V> {
+    HashMap::with_capacity_and_hasher(capacity, fxhash::FxBuildHasher::default())
 }
